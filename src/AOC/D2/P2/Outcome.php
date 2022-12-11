eabab7aad7392e0e574a399db6adba3a8af7594e
Michael Leach <michaelgeorgeleach@gmail.com>
2022-12-11 17:41:33 +0000

    Tidies comments / refactors enum method

--- src/AOC/D2/P2/Outcome.php
+++ src/AOC/D2/P2/Outcome.php
@@ -7,6 +7,11 @@ use AOC\D2\P1\Hand\Paper;
 use AOC\D2\P1\Hand\Rock;
 use AOC\D2\P1\Hand\Scissors;
 
+/**
+ * Class Outcome
+ *
+ * @package AOC\D2\P2
+ */
 enum Outcome: string
 {
    case LOSE = 'X';
@@ -27,30 +32,37 @@ enum Outcome: string
        ];
    }
 
+    /**
+     * For a given $hand, get the hand which should be played to achieve the outcome of the enum (win, draw or lose)
+     *
+     * @param Hand $hand    The hand that's been played
+     *
+     * @return Hand         The hand that should be played to achieve the outcome
+     */
    public function forHand(Hand $hand): Hand
    {
         return match ($this) {
             self::DRAW => $hand,
-            self::WIN => $this->losesAgainst($hand),
-            self::LOSE => $this->winsAgainst($hand)
+            self::WIN => $this->winLoseOutcome($hand, true),
+            self::LOSE => $this->winLoseOutcome($hand, false)
         };
    }
 
-   private function winsAgainst(Hand $hand): Hand
+    /**
+     * @param Hand $hand
+     * @param bool $shouldWin
+     *
+     * @return Hand
+     */
+   private function winLoseOutcome(Hand $hand, bool $shouldWin): Hand
    {
-        foreach ($this->getAllHands() as $check) {
-            if ($hand->defeats($check)) {
-                return $check;
-            }
-        }
-   }
+       foreach ($this->getAllHands() as $check) {
+           $a = $shouldWin ? $hand : $check;
+           $b = $shouldWin ? $check : $hand;
 
-    private function losesAgainst(Hand $hand): Hand
-    {
-        foreach ($this->getAllHands() as $check) {
-            if ($check->defeats($hand)) {
-                return $check;
-            }
-        }
-    }
+           if ($a->defeats($b)) {
+               return $check;
+           }
+       }
+   }
 }
--- src/AOC/D2/P2/RoundProvider/PartTwoRoundProvider.php
+++ src/AOC/D2/P2/RoundProvider/PartTwoRoundProvider.php
@@ -11,17 +11,26 @@ use AOC\D2\P1\RoundProviderInterface;
 use AOC\D2\P2\Outcome;
 use AOC\Helper\InputReader;
 
+/**
+ * Class PartTwoRoundProvider
+ *
+ * @package AOC\D2\P2
+ */
 class PartTwoRoundProvider implements RoundProviderInterface
 {
-    public function __construct(private string $filename)
+    /**
+     * @param string $filename
+     */
+    public function __construct(private readonly string $filename)
     {
     }
 
+    /**
+     * @return array
+     */
     public function provide(): array
     {
         $rounds = [];
-
-        $count = 0;
 
         foreach (array_filter(InputReader::fileToLines($this->filename)) as $line) {
             [$theirs, $desiredOutcome] = explode(' ', $line);
