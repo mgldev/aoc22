ea1427516287b0329d84aa567615d5ca607373d5
Michael Leach <michaelgeorgeleach@gmail.com>
2022-12-15 21:46:31 +0000

    Completes D10 P2 (messy, needs improvement)

--- src/AOC/D10/P2/solve.php
+++ src/AOC/D10/P2/solve.php
@@ -1,44 +1,51 @@
 <?php
 
 use AOC\Helper\InputReader;
-
 require_once __DIR__ . '/../../../../vendor/autoload.php';
 
 $cycle = 0;
 $x = 1;
 $crt = [];
-
 $crtLine = '';
+$pixel = 0;
 
 foreach (InputReader::fileToLines(__DIR__ . '/../input.txt') as $instruction) {
     $parts = explode(' ', $instruction);
     $command = $parts[0];
     $value = isset($parts[1]) ? (int) $parts[1] : null;
-    $sprite = [$x - 1, $x, $x + 1];
 
     switch ($command) {
         case 'noop':
-            $cycle += 1;
-            $crtLine .= in_array($cycle, $sprite) ? '#' : '.';
-            echo $cycle . "\n";
+            $cycle++;
+            $pixel = $cycle - (count($crt) * 40);
+            $sprite = [$x - 1, $x, $x + 1];
+            $crtLine .= in_array($pixel, $sprite) ? '#' : '.';
+            if (strlen($crtLine) == 40) {
+                $crt[] = $crtLine;
+                $crtLine = '';
+                if (count($crt) === 6) break;
+            }
             break;
 
         case 'addx':
             for ($i = 0; $i < 2; $i++) {
+                if ($i === 1) {
+                    $x += $value;
+                }
                 $cycle++;
-                echo $cycle . "\n";
-                $crtLine .= in_array($cycle, $sprite) ? '#' : '.';
-            }
-            $x += $value;
-            break;
-    }
+                $pixel = $cycle - (count($crt) * 40);
+                $sprite = [$x - 1, $x, $x + 1];
+                $crtLine .= in_array($pixel, $sprite) ? '#' : '.';
 
-    if (strlen($crtLine) == 40) {
-        echo "Rendering\n";
-        $crt[] = $crtLine;
-        $crtLine = '';
-        if (count($crt) === 6) break;
+                if (strlen($crtLine) == 40) {
+                    $crt[] = $crtLine;
+                    $crtLine = '';
+                    if (count($crt) === 6) break;
+                }
+            }
+            //$x += $value;
+            break;
     }
 }
 
-//echo implode("\n", $crt);
+echo implode("\n", $crt);
